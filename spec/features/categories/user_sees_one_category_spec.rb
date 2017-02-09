cb620d7ee8a098fd3c6e9f60cd45d258e33f56ae
Charlotte Moore <mooc123@gmail.com>
2017-02-09 16:20:45 -0700

    category show page displays all jobs with that category.

--- spec/features/categories/user_sees_one_category_spec.rb
+++ spec/features/categories/user_sees_one_category_spec.rb
@@ -2,10 +2,14 @@ require 'rails_helper'
 
 RSpec.describe "When user visits category show" do
   scenario "they see details for that category" do
+    company = Company.create(name: "ESPN")
     category = Category.create(title: 'Finance')
+    category.jobs.create(title: "Developer", city: "Denver", level_of_interest: 90, company: company)
 
     visit(category_path(category))
 
+    expect page.has_content?("Developer")
+    expect page.has_link?("Developer")
     expect page.has_content?("Finance")
   end
 end
